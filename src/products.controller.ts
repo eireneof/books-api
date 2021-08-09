710b49fc7aeb595c337b5b6c511764c07ec3ea6d
Eirene Fireman <eof@ic.ufal.br>
2021-08-09 19:01:23 -0300

    modelando um produto

--- src/products.controller.ts
+++ src/products.controller.ts
@@ -1,34 +1,37 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { Product } from "./product.model";
 
 @Controller('products')
 export class ProductsController {
+    products: Product[] = [
+        new Product("LIV01", "Livro 1", 29.90),
+        new Product("LIV02", "Livro 2", 39.90),
+        new Product("LIV03", "Livro 3", 29.90),
+    ];
 
     @Get()
-    findAll(): string {
-        return "Lista todos os produtos";
+    findAll(): Product[] {
+        return this.products;
     }
 
     @Get(':id')
-    findOne(@Param() params): string {
-        console.log(params.id);
-        return `Retorna os dados do produto ${params.id}`;
+    findOne(@Param() params): Product {
+        return this.products[0];
     }
 
     @Post()
-    create(@Body() produto): string {
-        console.log(produto);
-        return "Produto criado";
+    create(@Body() product: Product) {
+        product.id = 100;
+        this.products.push(product);
     }
 
     @Put()
-    editOne(@Body() produto): string {
-        console.log(produto);
-        return "Produto alterado";
+    editOne(@Body() product: Product) : Product{
+        return product;
     }
 
     @Delete(':id')
-    delete(@Param() params): string {
-        console.log(params.id);
-        return `Apaga o produto ${params.id}`;
+    delete(@Param() params){
+        return this.products.pop();
     }
 }
